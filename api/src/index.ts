cae314126dcefd5706b251fde0da3b6d213e10ec
Angeles <angi.zerep@gmail.com>
2022-01-17 21:54:53 -0400

    conf apollo

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,25 @@
-console.log('Yeah! this is working')
+import { ApolloServer } from 'apollo-server'
+
+// 1 - query
+    const typeDefs = `
+        type Query {
+            info: String!
+        }
+    `
+
+// 2 - resolvers
+    const resolvers = {
+        Query: {
+            info: () => 'This is the API'
+        }
+    }
+
+// 3 - init service
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers
+    })
+
+server.listen().then(({ url }) => console.log( `Server is running on ${url}`))
+
+console.log('Yeah! this is working xD')
